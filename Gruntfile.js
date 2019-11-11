a8750c7a2f09addfe771ff42469b2a2cec5ee1f6
Luuk Verhoeven <luuk@ldesign.nl>
2019-11-11 16:55:03 +0100

    Update Gruntfile for local runtime

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,16 @@
 "use strict";
 
 module.exports = function (grunt) {
+    // Running local with
+    // First time npm install
+    // nvm use 8.9
+    // grunt --moodledir=/Users/mail/OPENSOURCE/moodle-370/
 
     // We need to include the core Moodle grunt file too, otherwise we can't run tasks like "amd".
     require("grunt-load-gruntfile")(grunt);
-    grunt.loadGruntfile("../../Gruntfile.js");
+
+    var MOODLE_DIR = grunt.option('moodledir') || '../../';
+    grunt.loadGruntfile(MOODLE_DIR + "Gruntfile.js");
 
     //Load all grunt tasks.
     grunt.loadNpmTasks("grunt-contrib-less");
@@ -14,12 +20,46 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
         watch: {
+            // If any .less file changes in directory "less" then run the "less" task.
+            less: {
+                files: "less/*.less",
+                tasks: ["less"]
+            },
+            fixindent: {
+                files: "less/*.less",
+                tasks: ["fixindent"]
+            },
             amd: {
                 files: "amd/src/*.js",
                 tasks: ["amd"]
             }
         },
-
+        less: {
+            // Production config is also available.
+            development: {
+                options: {
+                    // Specifies directories to scan for @import directives when parsing.
+                    // Default value is the directory of the source, which is probably what you want.
+                    paths: ["less/"],
+                    compress: false
+                },
+                files: {
+                    "styles.css": "less/styles.less"
+                }
+            },
+        },
+        fixindent: {
+            stylesheets: {
+                src: [
+                    'styles.css'
+                ],
+                dest: 'styles.css',
+                options: {
+                    style: 'space',
+                    size: 4
+                }
+            }
+        },
         eslint: {
             amd: {src: "amd/src"}
         },
@@ -35,5 +75,5 @@ module.exports = function (grunt) {
     });
 
     // The default task (running "grunt" in console).
-    grunt.registerTask("default", [ "uglify"]);
+    grunt.registerTask("default", ["less", "fixindent", "eslint", "uglify"]);
 };
