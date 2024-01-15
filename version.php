d8b18749fd33fda58d60157d06d23cdc55f68139
Luuk Verhoeven <luuk@ldesign.nl>
2024-01-15 09:03:45 +0100

    Update version.php

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
  **/
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->release = '4.1.0 (2022112800)';
+$plugin->release = '4.1.1 (2024011500)';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->version = 2024010900;
+$plugin->version = 2024011500;
 $plugin->requires = 2015111602;
 $plugin->component = 'local_commander';
