7f7cac0d4bc4d050b322b136ab9e8cc2c458a6ce
Hamza Tamyachte <hamza.tamyachte@ldesignmedia.nl>
2026-03-30 15:36:15 +0100

    Update #86c7xvd2d - Fix JS backward compatibility for Moodle 4.0.x and restore version support

--- amd/src/settings.js
+++ amd/src/settings.js
@@ -23,7 +23,7 @@
  * @author    Luuk Verhoeven
  **/
 
-import {getString} from 'core/str';
+import Str from 'core/str';
 import Notification from 'core/notification';
 import Log from 'core/log';
 
@@ -37,7 +37,7 @@ function init() {
         return;
     }
 
-    getString('js:keycode_help', 'local_commander')
+    Str.get_string('js:keycode_help', 'local_commander')
         .then((message) => {
             el.insertAdjacentHTML('beforebegin', `
                 <div class="alert alert-info" id="key-monitor">
--- version.php
+++ version.php
@@ -28,6 +28,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->release = '5.0.1';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->version = 2025092600;
-$plugin->requires = 2022112800;
+$plugin->requires = 2015111602;
 $plugin->component = 'local_commander';
-$plugin->supported = [405, 501];
+$plugin->supported = [39, 501];
